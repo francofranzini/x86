a7afadc349d50c613f83c0fbc569b0bb30434230
Franco Franzini <francofranzini12@gmail.com>
2025-11-09 18:52:40 -0300

    12 in progress

---
+++ Ej12/ej12.s
@@ -0,0 +1,43 @@
+.data
+list: .long 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+length:  .byte 10
+format: .asciz "La suma es: %d \n"
+.text
+/*
+
+suma:
+  #PROLOGO
+  pushq %rbp
+  movq %rsp, %rbp
+
+  #Cuerpo de la funcion
+  xorq %rax, %rax
+  addq %rdi, %rax
+  addq %rsi, %rax  
+
+  #Epilogo
+  movq %rbp, %rsp
+  popq %rbp
+  ret
+*/
+.global main
+main:
+  
+  xor %rcx, %rcx
+  movb length, %cl
+  subb $1, %cl
+  xorq %rax, %rax
+
+  leaq list, %rbx  
+  sumar_arreglo:
+    addl (%ebx), %eax
+    addq $4, %rbx
+    loop sumar_arreglo
+  
+  leaq format, %rdi
+  movq %rax, %rsi
+  call printf  
+  xorq %rax, %rax
+  ret
+    
+.section .note.GNU-stack    
